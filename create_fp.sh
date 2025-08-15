464079f16e76123ec34f6232e9bad92d95501d02
Sean Crowell <seanmrcrowell@gmail.com>
2025-08-15 15:56:59 -0500

    prepping stilt runs

--- create_fp.sh
+++ create_fp.sh
@@ -10,8 +10,12 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user="sean@belumenus.com"
 
-eval "$(conda shell.bash hook)"
-cd /scratch/07351/tg866507/halo 
-conda activate stilt
+#eval "$(conda shell.bash hook)"
+cd /home/sean/sata/halo-nyc/stilt/stilt_code
+conda activate stilt2
 #Rscript r/run_stilt_nyc_hrrr.r --alt=${1} --file=${2} 
-Rscript r/run_stilt_nyc_wrfghgd02.r --alt=${1} --file=${2} 
+for alt in 50 150 250 500 750 1000 1500 2000 3000 5000 7500 10000;
+do
+    echo ${alt}
+    Rscript r/run_stilt_nyc_hrrr.r --alt=${alt} --file=${1} 
+done
